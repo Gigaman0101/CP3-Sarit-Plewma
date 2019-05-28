c69763c94efed29b40580d783594c7be844fa76a
Sarit <splewma2008@hotmail.com>
2019-05-28 21:14:52 +0700

    example4
    added score  fot each subject.

---
+++ Exercise4_Sarit_P.py
@@ -0,0 +1,10 @@
+Found = 60.64
+Gen = 80.22
+In = 13.55
+ComPro = 50.78
+
+print("              ---You Score---")
+print("Foundation English               :",Found,"point")
+print("General Business                 :",Gen,"point")
+print("Introduction to Computer Systems :",In,"point")
+print("Computer Programming             :",ComPro,"point")
