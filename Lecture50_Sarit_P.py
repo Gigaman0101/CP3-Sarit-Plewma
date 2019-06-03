c7dc0594a4625ac79acdf0b0fa092d3ada50af54
Sarit <splewma2008@hotmail.com>
2019-06-03 22:26:10 +0700

    lec50 added func
    added function 4

--- Lecture50_Sarit_P.py
+++ Lecture50_Sarit_P.py
@@ -1,3 +1,10 @@
 def Number(x,y,z):
     print(x+y*int(z/x))
+def plus(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def lineequation(x,y):
+    print(2*x-y)
 Number(23,12,55)
+lineequation(20,10)
